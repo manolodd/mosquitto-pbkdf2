b69e31c6a4b59ef2e826edf4c65d4e512c13bc85
Manuel Domínguez-Dorado <ingeniero@ManoloDominguez.com>
2020-12-12 18:14:45 +0100

    Minor changes

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ function doSomethingWhenPBKDF2PasswordIsCreated(pbkdf2Password) {
 // PASSWORD VERIFICATION TEST
 //We simulate that the following PBKDF2 has been queried from MySQL :-)
 //The next variable 'passwordFromMySQL' correspond to 'password'
-passwordFromMySQL = 'PBKDF2$sha256$901$j24KtOVCjYfsqfjL$bUIeZ0n39NuQ3MU+Y3pofaSsTRNlfang';
+var passwordFromMySQL = 'PBKDF2$sha256$901$j24KtOVCjYfsqfjL$bUIeZ0n39NuQ3MU+Y3pofaSsTRNlfang';
 
 // We verify two passwords (one incorrect and other correct).
 mosquittoPBKDF2.verifyCredentials('password', passwordFromMySQL, doSomethingAfterVerification);
